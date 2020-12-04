1d66006cf5de292c91cde471bc4419912c06a5fa
Sarah <42073869+sjproctor@users.noreply.github.com>
2020-12-04 13:05:16 -0800

    Initial commit

---
+++ code-challenges.js
@@ -0,0 +1,39 @@
+// ASSESSMENT 5: JavaScript Coding Practical Questions
+
+// --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+
+var secretCodeWord1 = "lackadaisical"
+// Expected output: "l4ck4d41s1c4l"
+var secretCodeWord2 = "gobbledygook"
+// Expected output: "g0bbl3dyg00k"
+
+
+
+
+
+// --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
+
+var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
+// Expected output: "Apple" "Banana" "Peach"
+
+
+
+
+
+// --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
+
+var hand1 = [5, 5, 5, 3, 3]
+// Expected output: true
+var hand2 = [5, 5, 3, 3, 4]
+// Expected output: false
+var hand3 = [5, 5, 5, 5, 4]
+// Expected output: false
+var hand4 = [3, 5, 3, 5, 3]
+// Expected output: true
+
+
+// STRETCH:
+var hand5 = ["K", 5, "K", 5, "K"]
+// Expected output: true
+var hand6 = ["A", "Q", "A", 5, 3]
+// Expected output: false
---
+++ ruby_challenges.rb
@@ -0,0 +1,45 @@
+# ASSESSMENT 5: Ruby Coding Practical Questions
+
+# --------------------1) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data about each bike object.
+
+
+
+
+
+# Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
+
+
+# -------------------2) Add a bell to the bike class and create the ability to ring the bell when the method is called.
+
+
+
+
+
+# Expected output example: my_bike.ring_bell => 'Ding ding!'
+
+
+# -------------------3) Add a speedometer to the Bike class. The speed should be initialized at 0.
+
+
+
+
+
+# Expected output example: my_bike.speed => 0
+
+
+# -------------------4) Add the ability to pedal faster. The pedal_faster method should increase the speed by a particular amount.
+
+
+
+
+
+# Expected output example: my_bike.pedal_faster 10 => 10
+
+
+# -------------------5) Add the ability to brake. The brake method should decrease the speed by a particular amount. The bike cannot go negative speeds.
+
+
+
+
+
+# Expected output example: my_bike.brake 15 => 0
